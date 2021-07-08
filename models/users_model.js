f0e1ff74a18724da0616144e2fffeef63fa3f1ff
gzh236 <47617174+gzh236@users.noreply.github.com>
2021-07-08 20:49:48 +0800

    initial work on users model, validation, controller

---
+++ models/users_model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  hash: { type: String, required: true, unique: true },
+});
+
+const UserModel = new mongoose.Model("User", userSchema);
+
+module.exports = { UserModel };
